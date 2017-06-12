cef0861094c2737392b21123cc3c457297c4b816
Joshua Frandsen <jfran@pdsf6.nersc.gov>
2017-06-12 10:40:01 -0700

    update setenv according to upstream

--- scripts/setenv_pdsf.sh
+++ scripts/setenv_pdsf.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 module load gcc
-<<<<<<< HEAD
-module load ROOT
-=======
 module load cmake
 module load boost
 module use /project/projectdirs/alice/software/modulefiles
@@ -14,7 +11,6 @@ pythia_mod=`module -t avail 2>&1 | grep pythia8 | tail -n 1`
 [ ! -z $pythia_mod ] && module load $pythia_mod
 fastjet_mod=`module -t avail 2>&1 | grep fastjet | tail -n 1`
 [ ! -z $fastjet_mod ] && module load $fastjet_mod
->>>>>>> upstream/master
 
 module list
 
@@ -46,17 +42,6 @@ XDIR=`dirname $XDIR`
 export JETTYDIR=$XDIR
 
 if [ -z $PATH ]; then
-<<<<<<< HEAD
-	export PATH=$JETTYDIR/bin:$JETTYDIR/scripts:$HOME/pythia8226/bin
-else
-	export PATH=$JETTYDIR/bin:$JETTYDIR/scripts:$HOME/pythia8226/bin:$PATH
-fi
-
-if [ -z $LD_LIBRARY_PATH ]; then
-    export LD_LIBRARY_PATH=$JETTYDIR/lib:$HOME/pythia8226/lib
-else
-    export LD_LIBRARY_PATH=$JETTYDIR/lib:$HOME/pythia8226/lib:$LD_LIBRARY_PATH
-=======
 	export PATH=$JETTYDIR/bin:$JETTYDIR/scripts
 else
 	export PATH=$JETTYDIR/bin:$JETTYDIR/scripts:$PATH
@@ -72,5 +57,4 @@ if [ -z $DYLD_LIBRARY_PATH ]; then
     export DYLD_LIBRARY_PATH=$JETTYDIR/lib
 else
     export DYLD_LIBRARY_PATH=$JETTYDIR/lib:$DYLD_LIBRARY_PATH
->>>>>>> upstream/master
 fi
